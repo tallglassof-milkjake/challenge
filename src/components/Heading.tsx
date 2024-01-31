df19a1714963b67b5d8753a8a6475a4b083025de
Jake Mackinlay <jakemackinlay@Jakes-MacBook-Air.local>
2024-01-31 19:43:24 +1030

    removed React because it isnt the problem

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 interface Props {
     headingSize: string;
     text: string;
--- src/pages/SignupView.tsx
+++ src/pages/SignupView.tsx
@@ -1,5 +1,3 @@
-import React from "react"
-
 const SignupView = () => {
     return (
         <div>SignupView</div>
