3fdee9dff562429cb8bea9d9f81cbda1b04f204c
Jake Mackinlay <jakemackinlay@Jakes-MacBook-Air.local>
2024-01-31 20:14:36 +1030

    removed un needed browserrouter import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './App.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
